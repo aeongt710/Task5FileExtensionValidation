252c4f9f346eeaa05ca4e8836d2a0a04fb7f7697
Muhammad Ahmad <unknowndonlion@gmail.com>
2022-06-02 13:36:34 +0500

    Add project files.

---
+++ Models/FileVM.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+using Task5FileExtensionValidation.Models.ValidationAttributes;
+
+namespace Task5FileExtensionValidation.Models
+{
+    public class FileVM
+    {
+        [Required]
+        [FileExtens(".txt", ".docx")]
+        [Display(Name = "Single File Upload")]
+        public IFormFile SingleFile { get; set; }
+
+
+        [Required]
+        [FileExtens(".txt", ".docx")]
+        [Display(Name = "Multiple File Upload")]
+        public IFormFile[] MultipleFiles { get; set; }
+    }
+}
---
+++ Models/ValidationAttributes/FileExtensAttribute.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Task5FileExtensionValidation.Models.ValidationAttributes
+{
+    public class FileExtensAttribute : ValidationAttribute
+    {
+        private string[] Extensions { get; set; }
+        public FileExtensAttribute(params string[] _extens)
+        {
+            Extensions = _extens;
+        }
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value != null)
+            {
+                string _errors = "";
+                if (value is IEnumerable)
+                {
+                    var list = value as IEnumerable<object>;
+
+                    foreach (IFormFile item in list)
+                    {
+                        bool validateFile = false;
+                        foreach (string extension in Extensions)
+                        {
+                            if (item.FileName.EndsWith(extension))
+                            {
+                                validateFile = true;
+                                break;
+                            }
+                        }
+                        if (!validateFile)
+                            _errors = _errors + item.FileName + " contains Invalid Extension. ";
+                    }
+                    if (_errors == "")
+                        return ValidationResult.Success;
+                    else
+                        return new ValidationResult(_errors);
+                }
+                else if (value is IFormFile)
+                {
+                    var item = value as IFormFile;
+                    bool validateFile = false;
+                    foreach (string extension in Extensions)
+                    {
+
+                        if (item.FileName.EndsWith(extension))
+                        {
+                            validateFile = true;
+                            break;
+                        }
+                    }
+                    if (!validateFile)
+                        _errors = item.FileName + " contains Invalid Extension. ";
+
+                    if (_errors == "")
+                        return ValidationResult.Success;
+                    else
+                        return new ValidationResult(_errors);
+                }
+
+            }
+
+            return new ValidationResult("Value is null");
+        }
+        public FileExtensAttribute()
+        {
+
+        }
+    }
+}
